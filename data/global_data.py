597f6a9e90964740703d16d887db883548137942
Antoine BLANOT <antoineblanot@gmail.com>
2023-05-30 11:41:22 +0900

    add script for global data

---
+++ data/global_data.py
@@ -0,0 +1,53 @@
+from datasets import load_dataset
+
+
+def tweet_eval():
+    data = load_dataset('tweet_eval', name='sentiment', split='validation')
+
+    id2label = {'0': 'negative', '1': 'neutral', '2': 'positive'}
+    candidates_labels = ['negative', 'neutral', 'positive']
+
+    data = data.add_column(
+        'label_name',
+        [id2label[str(l)] for l in data['label']]
+    )
+    data = data.add_column(
+        'candidate_labels',
+        [candidates_labels] * len(data)
+    )
+    data.to_json('tweet_eval.json')
+
+
+def tweet_sentiment_extraction():
+    data = load_dataset('mteb/tweet_sentiment_extraction', split='test')
+
+    id2label = {'0': 'negative', '1': 'neutral', '2': 'positive'}
+    candidates_labels = ['negative', 'neutral', 'positive']
+
+    data = data.add_column(
+        'label_name',
+        [id2label[str(l)] for l in data['label']]
+    )
+    data = data.add_column(
+        'candidate_labels',
+        [candidates_labels] * len(data)
+    )
+    data.to_json('tweet_sentiment_extraction.json')
+
+def sst2():
+    data = load_dataset('glue', name='sst2', split='validation')
+
+    id2label = {'0': 'negative', '1': 'positive'}
+    candidates_labels = ['negative', 'positive']
+
+    data = data.add_column(
+        'label_name',
+        [id2label[str(l)] for l in data['label']]
+    )
+    data = data.add_column(
+        'candidate_labels',
+        [candidates_labels] * len(data)
+    )
+    data.to_json('sst2.json')
+
+sst2()
