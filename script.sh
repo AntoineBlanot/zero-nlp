ef2067ae33eb030870af2345a55421b377ef14e0
MAEDA Chikara <antoineblanot@gmail.com>
2023-05-31 11:07:48 +0900

    script for bert

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 MODEL_PATH="/home/chikara/ws/efficient-llm/exp/roberta-nli/checkpoint-70000"
 BS=8
-DO_MULTICLASS=True
-THRESHOLD=0.8
+DO_MULTICLASS=False
+THRESHOLD=...
 
 python classify.py \
     --model_path $MODEL_PATH --true_class "entailment" --false_class "contradiction" \
@@ -58,3 +58,37 @@ python classify.py \
     --data_name "demos-sentiment" \
     --seq_length 512 \
     --out_file "demos-sentiment"
+
+## GLOBAL
+
+python classify.py \
+    --model_path $MODEL_PATH --true_class "entailment" --false_class "contradiction" \
+    --task "global-boolqa" --bs $BS \
+    --do_multiclass $DO_MULTICLASS --threshold $THRESHOLD \
+    --data_name "global-boolqa" \
+    --seq_length 512 \
+    --out_file "global-boolqa"
+
+python classify.py \
+    --model_path $MODEL_PATH --true_class "entailment" --false_class "contradiction" \
+    --task "global-sentiment" --bs $BS \
+    --do_multiclass $DO_MULTICLASS --threshold $THRESHOLD \
+    --data_name "global-sentiment" \
+    --seq_length 512 \
+    --out_file "global-sentiment"
+
+python classify.py \
+    --model_path $MODEL_PATH --true_class "entailment" --false_class "contradiction" \
+    --task "global-paraphrase" --bs $BS \
+    --do_multiclass $DO_MULTICLASS --threshold $THRESHOLD \
+    --data_name "global-paraphrase" \
+    --seq_length 512 \
+    --out_file "global-paraphrase"
+
+python classify.py \
+    --model_path $MODEL_PATH --true_class "entailment" --false_class "contradiction" \
+    --task "global-nli" --bs $BS \
+    --do_multiclass $DO_MULTICLASS --threshold $THRESHOLD \
+    --data_name "global-nli" \
+    --seq_length 512 \
+    --out_file "global-nli"
